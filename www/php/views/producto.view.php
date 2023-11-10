0deacd0b49dbe3328399de46776e3f3bb4cafe2a
OiherOleaga <oiher.oleaga@ikasle.egibide.org>
2023-11-10 10:26:42 +0100

    develop-verEmpresa

--- www/php/views/producto.view.php
+++ www/php/views/producto.view.php
@@ -5,27 +5,36 @@ require("./mapa.php"); ?>
 </style>
 <div class="pagina">
     <div class="verProductoCaja">
-        <div class="vendedor">
-            <div class="avatar">
-                <img src=<?= $logoEmpresa ?> class="avatarEmpresa">
-                <p><?= $empresa ?></p>
-            </div>
-            <img src=<?= $imgLike ?> class="corazon">
+        <a href="verEmpresa.php">
+            <div class="vendedor">
+                <div class="avatar">
+                    <img src=<?= $logoEmpresa ?> class="avatarEmpresa">
+                    <p><?= $empresa ?></p>
+                </div>
+        </a>
+
+        <img src=<?= $imgLike ?> class="corazon">
+    </div>
+    <div class="producto">
+        <div class="foto"><img src=<?= $imagen ?>></div>
+        <div class="titulo">
+            <p><?= $titulo ?></p>
         </div>
-        <div class="producto">
-            <div class="foto"><img src=<?= $imagen ?>></div>
-            <div class="titulo"><p><?= $titulo ?></p></div>
-            <div class="precio"><h5><?= $precio . "€" ?></h5></div>
-            <div class="desc"><p><?= $desc ?></p></div>
+        <div class="precio">
+            <h5><?= $precio . "€" ?></h5>
+        </div>
+        <div class="desc">
+            <p><?= $desc ?></p>
         </div>
     </div>
-    <div class="mapa">
-        <h2>Localicazi&oacute;n del local</h2>
-        <div id="map">
-            <p id="latitud"> <?= $latitude ?> </p>
-            <p id="longi"> <?= $longitude ?> </p>
-        </div>
+</div>
+<div class="mapa">
+    <h2>Localicazi&oacute;n del local</h2>
+    <div id="map">
+        <p id="latitud"> <?= $latitude ?> </p>
+        <p id="longi"> <?= $longitude ?> </p>
     </div>
+</div>
 </div>
 <script src="/js/scriptMapa.js"></script>
 <?php require("../php/views/partials/footer.php") ?>
--- www/php/views/verEmpresa.view.php
+++ www/php/views/verEmpresa.view.php
@@ -3,35 +3,7 @@ require("./mapa.php"); ?>
 <style>
     <?php include('../css/verEmpresa.css') ?>
 </style>
-<div class="pagina">
-    <div class="verProductoCaja">
-        <div class="vendedor">
-            <div class="avatar">
-                <img src=<?= $logoEmpresa ?> class="avatarEmpresa">
-                <p><?= $empresa ?></p>
-            </div>
-            <img src=<?= $imgLike ?> class="corazon">
-        </div>
-        <div class="producto">
-            <div class="foto"><img src=<?= $imagen ?>></div>
-            <div class="titulo">
-                <p><?= $titulo ?></p>
-            </div>
-            <div class="precio">
-                <h5><?= $precio . "€" ?></h5>
-            </div>
-            <div class="desc">
-                <p><?= $desc ?></p>
-            </div>
-        </div>
-    </div>
-    <div class="mapa">
-        <h2>Localicazi&oacute;n del local</h2>
-        <div id="map">
-            <p id="latitud"> <?= $latitude ?> </p>
-            <p id="longi"> <?= $longitude ?> </p>
-        </div>
-    </div>
-</div>
+
+
 <script src="/js/scriptMapa.js"></script>
 <?php require("../php/views/partials/footer.php") ?>
