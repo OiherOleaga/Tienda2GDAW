b5a8058262440b250c023f45c9cbb9b19db9005f
OiherOleaga <oiher.oleaga@ikasle.egibide.org>
2023-11-13 13:30:06 +0100

    Conf Empresa

---
+++ www/php/confEmpresa.php
@@ -0,0 +1,15 @@
+<?php
+require("baseDeDatos.php");
+if (isset($_COOKIE[session_name()])) {
+    session_start();
+    $id = $_SESSION["id"];
+    $data = ['id' => $id];
+    $cliente = getCliente($data);
+    require("views/partials/headUsuario.php");
+} else require("views/partials/headInicio.php");
+
+
+$id = isset($_GET["idEmpresa"]) ? $_GET["idEmpresa"] : "";
+$data = ['id' => $id];
+$empresa = consultarEmpresa($data);
+require "views/confEmpresa.view.php";
