25c764e2823de8b0c51f6e631585bb5ffdbf00f5
OiherOleaga <oiher.oleaga@ikasle.egibide.org>
2023-11-17 09:23:42 +0100

    node Pagar arreglado

--- www/php/contact.php
+++ www/php/contact.php
@@ -4,12 +4,12 @@ if (isset($_COOKIE[session_name()])) {
     require "../db/clientes.php";
     session_start();
     if (!isset($_SESSION["id"])) {
-        setcookie("PHPSESSID", "", time() - 3600); 
+        setcookie("PHPSESSID", "", time() - 3600);
     }
 
     $cliente = getCliente($_SESSION["id"]);
     if ($cliente == null) {
-        setcookie("PHPSESSID", "", time() - 3600); 
+        setcookie("PHPSESSID", "", time() - 3600);
         require("views/partials/headInicio.php");
     } else {
         require("views/partials/headUsuario.php");
