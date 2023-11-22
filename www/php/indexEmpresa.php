89fa0daa8b09b3d96862006d1d318dfd12b336b0
Anartz <anartz.pagaldai@ikasle.egibide.org>
2023-11-22 19:09:53 +0100

    atualizazion cliente a medias

--- www/php/indexEmpresa.php
+++ www/php/indexEmpresa.php
@@ -4,21 +4,26 @@ $empresa = $cliente;
 $errorDev = "";
 $mensajeUsuario = "";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-require "php/methods.php";
-require "php/preparacionUsuario.php";
-try {
-    $usuario = preparacionUsuarioUpdate("comerciante", $mensajeUsuario);
-    if ($usuario != null) {
-        updateComerciante($usuario, $empresa["ID"]);
-        $mensajeUsuario = "datos actualizados";
-        foreach ($usuario as $key => $valor) {
-            $empresa[ucfirst($key)] = $valor;
+    require "php/methods.php";
+    require "php/preparacionUsuario.php";
+    try {
+        $usuario = preparacionUsuarioUpdate("comerciante", $mensajeUsuario);
+        if ($usuario != null) {
+            if (isset($usuario["username"])) {
+                $usuario["nombre_empresa"] = $usuario["username"];
+                unset($usuario["username"]);
+            }
+
+            updateComerciante($usuario, $empresa["ID"]);
+            $mensajeUsuario = "datos actualizados";
+            foreach ($usuario as $key => $valor) {
+                $empresa[ucfirst($key)] = $valor;
+            }
         }
+    } catch (Exception $e) {
+        $errorUsuario = "error al cambair los datos";
+        $errorDev = $e->getMessage();
     }
-} catch (Exception $e) {
-    $errorUsuario = "error al cambair los datos";
-    $errorDev = $e->getMessage();
-}
 }
 
 $productos = getProductosComerciante($empresa["ID"]);
--- www/php/views/perfilUsuario.view.php
+++ www/php/views/perfilUsuario.view.php
@@ -81,5 +81,5 @@
         </div>
     </div>
 </div>
-<script src="/js/verPerfil.js"></script>
+<script type="module" src="/js/verPerfil.js"></script>
 <?php require("partials/footer.php") ?>
