f1de3e2431e89342e4a8e3ac6b74619f50b945dd
David Moreno Alonso <davizmoreno240@gmail.com>
2023-11-25 18:27:47 +0100

    quitado comentario en cookies js

--- www/js/aceptarCookies.js
+++ www/js/aceptarCookies.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     if (document.cookie.indexOf("aceptarCookies=true") === -1) {
         setTimeout(function () {
             document.getElementById("cookies").style.display = "flex";
-        }, 1000); // Cambia esto según tus necesidades
+        }, 1000);
     }else document.getElementById("cookies").style.display = "none";
 
 });
