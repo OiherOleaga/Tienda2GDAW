c8d9420be83bffc94cdb25b7ab139a774415dad6
David Moreno Alonso <davizmoreno240@gmail.com>
2023-11-25 18:35:02 +0100

    pointer events en cookies

--- www/js/aceptarCookies.js
+++ www/js/aceptarCookies.js
@@ -1,14 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
+
     if (document.cookie.indexOf("aceptarCookies=true") === -1) {
         setTimeout(function () {
             document.getElementById("cookies").style.display = "flex";
-        }, 1000);
-    }else document.getElementById("cookies").style.display = "none";
-
+            document.body.style.pointerEvents = "none";
+        }, 500);
+    }
 });
 
 document.getElementById("aceptarCookies").addEventListener("click", aceptarCookies)
 function aceptarCookies() {
     document.cookie = "aceptarCookies=true; expires=" + new Date(new Date().getTime() + 365 * 24 * 60 * 60 * 1000).toUTCString();
     document.getElementById("cookies").style.display = "none";
+    document.body.style.pointerEvents = "all";
 }
